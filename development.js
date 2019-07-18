376ffb003be453d3447e2857939b56fd1ebfdbb9
qingwen <dongqingwen@lanjinrong.com>
2019-07-18 17:56:29 +0800

    格式化

--- development.js
+++ development.js
@@ -3,10 +3,10 @@ const Application = require('thinkjs');
 const watcher = require('think-watcher');
 
 const instance = new Application({
-  ROOT_PATH: __dirname,
-  APP_PATH: path.join(__dirname, 'src'),
-  watcher: watcher,
-  env: 'development'
+    ROOT_PATH: __dirname,
+    APP_PATH: path.join(__dirname, 'src'),
+    watcher: watcher,
+    env: 'development'
 });
 
 instance.run();
--- src/common/config/extend.js
+++ src/common/config/extend.js
@@ -5,17 +5,17 @@ const cache = require('think-cache');
 const session = require('think-session');
 
 module.exports = [
-  view, // make application support view
-  model(think.app),
-  cache,
-  session,
-  {
-    think: {
-      TMPDIR_PATH: path.join(think.ROOT_PATH, 'runtime', 'tmp'),
-      RUNTIME_PATH: path.join(think.ROOT_PATH, 'runtime'),
-      RESOURCE_PATH: path.join(think.ROOT_PATH, 'www'),
-      UPLOAD_PATH: path.join(think.ROOT_PATH, 'www', 'static/upload'),
-      UPLOAD_BASE_URL: ''
+    view, // make application support view
+    model(think.app),
+    cache,
+    session,
+    {
+        think: {
+            TMPDIR_PATH: path.join(think.ROOT_PATH, 'runtime', 'tmp'),
+            RUNTIME_PATH: path.join(think.ROOT_PATH, 'runtime'),
+            RESOURCE_PATH: path.join(think.ROOT_PATH, 'www'),
+            UPLOAD_PATH: path.join(think.ROOT_PATH, 'www', 'static/upload'),
+            UPLOAD_BASE_URL: ''
+        }
     }
-  }
 ];
